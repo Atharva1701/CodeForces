05d5f0bdf0fbd55eeef5773ac243aaab5836f066
Atharva1701 <patil.atharva1701@gmail.com>
2021-01-10 16:58:57 +0530

    A problems

---
+++ 1370A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+void gcd_test_cases()
+{
+  int n;
+  scanf("%d",&n);
+
+  std :: cout << n / 2 << std :: endl;
+}
+int main()
+{
+  int tt;
+  scanf("%d",&tt);
+  while(tt--) {
+    gcd_test_cases();
+  }
+}
---
+++ 1371A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+void test_cases()
+{
+  int n;
+  scanf("%d",&n);
+  std :: cout << (n + 1) / 2 << std :: endl;
+}
+int main()
+{
+  int tt;
+  scanf("%d",&tt);
+
+  while(tt--) {
+    test_cases();
+  }
+}
