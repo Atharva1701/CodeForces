70583df6989b6bee43d1da59760f03ae23942566
Atharva1701 <patil.atharva1701@gmail.com>
2021-02-01 20:43:04 +0530

    A problem

---
+++ 1325A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+void test_cases()
+{
+  int x;
+  scanf("%d",&x);
+  std :: cout << 1 << ' ' << x - 1 << std :: endl;
+}
+int main()
+{
+  int tt;
+  scanf("%d",&tt);
+  while(tt--){
+    test_cases();
+  }
+}
