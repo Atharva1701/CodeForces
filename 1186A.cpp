409d18e19e5f2d1c0d385b853b816cec9e014701
Atharva1701 <patil.atharva1701@gmail.com>
2021-02-04 21:56:19 +0530

    A problem

---
+++ 1186A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+int main()
+{
+  int n,pens,nb;
+  std :: cin >> n >> pens >> nb;
+
+  if(pens >= n && nb >= n)
+    std :: cout << "Yes";
+  else
+    std :: cout << "No";
+
+}
